490c0ef9014c966dab05a8130e709b51aeb612b5
Daniel Rabstejnek <rabstejnek@gmail.com>
2024-05-28 17:52:40 -0400

    Move express import to create_app

--- src/deduper/app/deploy.py
+++ src/deduper/app/deploy.py
@@ -1,8 +1,8 @@
-from shiny.express import wrap_express_app
 from pathlib import Path
 
 
 def create_app():
+    from shiny.express import wrap_express_app
     return wrap_express_app((Path(__file__).parent / "app.py").resolve())
 
 app = create_app()
