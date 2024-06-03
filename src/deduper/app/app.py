f7ee373b08ce40e5b1024467795deac5600fbaf3
Daniel Rabstejnek <rabstejnek@gmail.com>
2024-06-03 13:23:51 -0400

    Refactored code, added ID links, added help text, added second pagination bar

--- src/deduper/app/app.py
+++ src/deduper/app/app.py
@@ -6,12 +6,12 @@ from deduper.app.constants import DEDUPER_MAP, PAGE_SIZE, ANNOTATE_CHOICES
 import pandas as pd
 import io
 import math
+from deduper.app.icons import question_circle_fill
 
 # TODO
-# Choose RIS file - wording
-# Add year to table
-# Add selector for DB, link ID
-# Select multiple of columns to show in table, default to year, title, abstract
+# Select multiple of columns to show in table, default to id, title, abstract
+# Add dynamic deduper creation (ie calculate columns after file upload, then have ui to select them and type of deduplication)
+#     once this is done, add selector for ID column to work with ID links
 # Bug: check radio box, turn annotate off, turn annotate back on; check didn't persist
 #     annotations only save when page changes or download clicked?
 
@@ -19,10 +19,21 @@ import math
 annotations = reactive.value([])
 page = reactive.value(1)
 
+ID_URL_MAP ={
+"---":None,
+"PMID":"https://pubmed.ncbi.nlm.nih.gov/{id}/",
+"WoS":"https://www.webofscience.com/wos/woscc/full-record/{id}",
+"HERO":"https://heronet.epa.gov/heronet/index.cfm/reference/details/reference_id/{id}",
+"DOI":"https://doi.org/{id}"}
+
+
+
+
 core_ui.card(
     core_ui.row(
-        core_ui.column(6,ui.input_file("file","Choose file")),
-        core_ui.column(6,ui.input_select("deduper_select","Choose deduper",{"id_and_title":"ID and title","id_only":"ID only","title_only":"Title only"}))
+        core_ui.column(4,ui.input_file("file","Choose RIS file")),
+        core_ui.column(4,ui.input_select("deduper_select",core_ui.tooltip(ui.span("Choose deduper ",question_circle_fill),"Deduplication will be done based on the chosen criteria (mixture of ID and title)",placement="right",id="deduper_select_tooltip"),{"id_and_title":"ID and title","id_only":"ID only","title_only":"Title only"})),
+        core_ui.column(4,ui.input_select("id_url_select",core_ui.tooltip(ui.span("Show ID url ",question_circle_fill),"If a selection is made, the IDs shown in the computed table will link to their respective database URL.",placement="right",id="id_url_select_tooltip"),{k:k for k in ID_URL_MAP.keys()}))
         ),
     core_ui.card_footer(
         core_ui.row(
@@ -65,6 +76,8 @@ def annotations_slice():
 def set_annotations():
     annotations.set([0]*len(records()))
 
+ui.div(id="pagination_container_0")
+
 ui.div(id="results_container")
 
 def download_results():
@@ -98,7 +111,7 @@ def render_download():
 
 
 @reactive.effect
-@reactive.event(results_slice,input["annotate"])
+@reactive.event(results_slice,input["annotate"],input["id_url_select"])
 def render_results():
     ui.remove_ui("#results_container *")
     cards = []
@@ -120,30 +133,26 @@ def render_results():
             for y,data in enumerate(result,len(ANNOTATE_CHOICES)):
                 checked = {"checked":"checked"} if y == selected else {}
                 radio_button = ui.tags.input(value=y,type="radio",name=id,**checked)
-                id_div = ui.div(ui.div("ID",{"class":"fw-bold"}),data.get("id",""),{"class":"overflow-auto","style":"max-height:150px;"})
-                title_div = ui.div(ui.div("Title",{"class":"fw-bold"}),data.get("title",""),{"class":"overflow-auto","style":"max-height:150px;"})
-                abstract_div = ui.div(ui.div("Abstract",{"class":"fw-bold"}),ui.div(data.get("abstract",""),{"class":"overflow-auto","style":"max-height:150px;"}))
-                row = core_ui.row(
-                    core_ui.column(1,radio_button),
-                    core_ui.column(3,id_div),
-                    core_ui.column(3,title_div),
-                    core_ui.column(5,abstract_div),
-                    )
+                cols = [core_ui.column(1,radio_button)]
+                for col_name in [("id","ID",1),("year","Year",1),("title","Title",3),("authors","Authors",2),("abstract","Abstract",4)]:
+                    col_contents = ui.div({"class":"overflow-auto","style":"max-height:150px;"},ui.div({"class":"fw-bold"},col_name[1]),data.get(col_name[0],""))
+                    cols.append(ui.div({"class":f"col-sm-{col_name[2]}"},col_contents))
+                row = core_ui.row(*cols)
                 rows.append(row)
 
         else:
 
             for data in result:
-                id_div = ui.div(ui.div("ID",{"class":"fw-bold"}),data.get("id",""),{"class":"overflow-auto","style":"max-height:150px;"})
-                title_div = ui.div(ui.div("Title",{"class":"fw-bold"}),data.get("title",""),{"class":"overflow-auto","style":"max-height:150px;"})
-                abstract_div = ui.div(ui.div("Abstract",{"class":"fw-bold"}),ui.div(data.get("abstract",""),{"class":"overflow-auto","style":"max-height:150px;"}))
-                row = core_ui.row(
-                    core_ui.column(3,id_div),
-                    core_ui.column(3,title_div),
-                    core_ui.column(6,abstract_div),
-                    )
+                cols = []
+                for col_name in [("id","ID",1),("year","Year",1),("title","Title",3),("authors","Authors",3),("abstract","Abstract",4)]:
+                    value = data.get(col_name[0],"")
+                    if col_name[0] == "id" and (url:=ID_URL_MAP.get(input["id_url_select"]())) is not None:
+                        value = ui.a(value,target="_blank",href=url.format(id=value))
+                    col_contents = ui.div({"class":"overflow-auto","style":"max-height:150px;"},ui.div({"class":"fw-bold"},col_name[1]),value)
+                    cols.append(ui.div({"class":f"col-sm-{col_name[2]}"},col_contents))
+                row = core_ui.row(*cols)
                 rows.append(row)
-        card = core_ui.card(*rows,id=id,class_="shiny-input-radiogroup")
+        card = core_ui.card(ui.div({"class":"container"},*rows),id=id,class_="shiny-input-radiogroup")
         cards.append(card)
     if not cards:
         cards = [ui.p("No duplicates found!")]
@@ -158,52 +167,56 @@ def update_annotations():
     return
 
 
-ui.div(id="pagination_container")
+ui.div(id="pagination_container_1")
 
 @reactive.effect
 def render_pagination():
-    ui.remove_ui("#pagination_container *")
-    components = []
-    if page() > 1:
-        components.extend(
-            [
-                ui.input_action_link(id="page_start",label="<<"),
-                " ",
-                ui.input_action_link(id="page_down",label="<"),
-                " ",
-            ]
-        )
-    components.append(f"Page {page()}")
-    if page() < len(results())/PAGE_SIZE:
-        components.extend(
-            [
-                " ",
-                ui.input_action_link(id="page_up",label=">"),
-                " ",
-                ui.input_action_link(id="page_end",label=">>"),
-            ]
-        )
+    ui.remove_ui("#pagination_container_0 *")
+    ui.remove_ui("#pagination_container_1 *")
+    def _render(id_suffix,*args):
+        components = []
+        if page() > 1:
+            components.extend(
+                [
+                    ui.input_action_link(id=f"page_start_{id_suffix}",label="<<"),
+                    " ",
+                    ui.input_action_link(id=f"page_down_{id_suffix}",label="<"),
+                    " ",
+                ]
+            )
+        components.append(f"Page {page()}")
+        if page() < len(results())/PAGE_SIZE:
+            components.extend(
+                [
+                    " ",
+                    ui.input_action_link(id=f"page_up_{id_suffix}",label=">"),
+                    " ",
+                    ui.input_action_link(id=f"page_end_{id_suffix}",label=">>"),
+                ]
+            )
+        return ui.div(*components,*args)
 
-    ui.insert_ui(ui.div(*components),"#pagination_container")
+    ui.insert_ui(_render(0,{"class":"mb-3"}),"#pagination_container_0")
+    ui.insert_ui(_render(1),"#pagination_container_1")
 
 
 @reactive.effect
-@reactive.event(input["page_start"])
+@reactive.event(input["page_start_0"],input["page_start_1"])
 def page_start():
     page.set(1)
 
 @reactive.effect
-@reactive.event(input["page_down"])
+@reactive.event(input["page_down_0"],input["page_down_1"])
 def page_down():
     page.set(page()-1)
 
 @reactive.effect
-@reactive.event(input["page_up"])
+@reactive.event(input["page_up_0"],input["page_up_1"])
 def page_up():
     page.set(page()+1)
 
 @reactive.effect
-@reactive.event(input["page_end"])
+@reactive.event(input["page_end_0"],input["page_end_1"])
 def page_end():
     page.set(math.ceil(len(results())/PAGE_SIZE))
 
